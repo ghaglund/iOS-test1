c98eb66b140fe7840d3e27bac8ac0c1848b0f277
ghaglund <ghaglund@upsales.com>
2017-01-16 14:44:16 +0100

    uppdatering
    uppdatering

--- iOS-test/ViewController.swift
+++ iOS-test/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBAction func tabButton(_ sender: UIButton) {
     }
     @IBOutlet weak var displayLabel2: UILabel!
+    @IBOutlet weak var inputField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

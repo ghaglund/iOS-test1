f432af5c11552c1d33295cd4653f74981aa52313
ghaglund <ghaglund@upsales.com>
2017-01-17 13:25:32 +0100

    Update
    Update

--- iOS-test/ViewController.swift
+++ iOS-test/ViewController.swift
@@ -13,10 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
     @IBAction func tabButton(_ sender: UIButton) {
     }
+    @IBOutlet weak var displayLabel2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         displayLabel.text = NSLocalizedString("Hello World!", comment: "What does the fox say?")
+        displayLabel2.text = NSLocalizedString("Life!", comment: "What would you say about life?")
     }
 
     override func didReceiveMemoryWarning() {
